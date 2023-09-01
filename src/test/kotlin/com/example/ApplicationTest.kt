0ef749e49e80c4428e8a59f7fe11c4c784c76465
Pavel Zmushko <p.zmushko@besmart.by>
2023-09-01 22:57:36 +0300

    minor commit

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -2,6 +2,7 @@ package com.example
 
 import com.example.utils.removeImage
 import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -10,11 +11,15 @@ class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
         application {
-
-        }
-        client.get("/").apply {
             val result = removeImage("files/", "png8f40aa74-f10b-44ff-a292-cbdafba99414.png")
             assertEquals(result, true)
+        }
+    }
+
+    @Test
+    fun testHello() = testApplication {
+        client.get("mushroom/getAll").apply {
+            assertEquals(this.status, HttpStatusCode.NotFound)
         }
     }
 }
